948cb162c0c610bab3c796a6bbad32a4cde5807e
juan <jugom123@gmail.com>
2021-05-28 10:04:17 +0200

    subida

---
+++ micro_jenkins/src/main/java/com/example/demo/MicroJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroJenkinsApplication.class, args);
+	}
+
+}
